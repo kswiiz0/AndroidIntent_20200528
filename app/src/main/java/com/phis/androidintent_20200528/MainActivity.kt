5dd5a65454c903e22cdbf091f801cf7c9096a69c
kswiiz0 <kswiiz0@gmail.com>
2020-05-28 20:47:07 +0900

    강제로 지정한 번호로 전화걸기 ( DIAL )  - 통화버튼 누리기 직전

--- app/src/main/java/com/phis/androidintent_20200528/MainActivity.kt
+++ app/src/main/java/com/phis/androidintent_20200528/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.phis.androidintent_20200528
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialBtn.setOnClickListener {
+            val phoneNum = phoneNumEdt.text.toString()
+            Log.d("입력받은 폰번호", phoneNum)
+
+
+            val myUri = Uri.parse("tel:01051604533")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+
+
+
+        }
     }
 }
