f1fd9bb69a46b58a565315fe3e221ce340bcbaba
kswiiz0 <kswiiz0@gmail.com>
2020-05-28 21:04:25 +0900

    바로 전화 연결 시도  > 권한 획득을 하지 못해서 강제종료됨.

--- app/src/main/java/com/phis/androidintent_20200528/MainActivity.kt
+++ app/src/main/java/com/phis/androidintent_20200528/MainActivity.kt
@@ -17,13 +17,24 @@ class MainActivity : AppCompatActivity() {
             val phoneNum = phoneNumEdt.text.toString()
             Log.d("입력받은 폰번호", phoneNum)
 
-
             val myUri = Uri.parse("tel:${phoneNum}")
             val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+        }
+
+        callBtn.setOnClickListener {
+            val phoneNum = phoneNumEdt.text.toString()
+            Log.d("입력받은 폰번호", phoneNum)
+
+            val myUri = Uri.parse("tel:${phoneNum}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+
+            //권한 얻는 작업을 하지 않았기때문에 에러로 표시가 되고, 실제 기능 실행시 앱오류가 발생한다.
             startActivity(myIntent)
 
 
-
         }
+
     }
 }
